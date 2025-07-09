5aa881caa35e4017ededfaa00d22e3c944c59553
Chad Redmond <credmond@mercyhurst.edu>
2025-07-09 18:13:49 +0000

    Fix deployment with relative paths and simplified routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/relations/',
-  build: {
-    assetsDir: 'assets',
-  },
+  base: './',
   server: {
     port: 3000,
     open: true
